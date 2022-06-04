6358e18eea1dca6c0df11b29c2693367f23beb97
LinhBui88 <87442413+Linhbui88@users.noreply.github.com>
2022-06-03 20:41:10 -0400

    setup and config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./*html'],
+  theme: {
+    screen: {
+      sm: '480px',
+      mg: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Josefin Sans', 'sans-serif'],
+        alanta: ['Alanta'],
+      },
+    },
+  },
+  plugins: [],
+}
